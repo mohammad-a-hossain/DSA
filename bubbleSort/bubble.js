1c909671f07e6542aa58d2d6f6f5abf93b385f71
mdabu-hossain <aabu.hossain.md@gmail.com>
2022-11-21 17:23:48 +0600

    add selection sort

---
+++ bubbleSort/bubble.js
@@ -0,0 +1,18 @@
+let arr=[1,4,5,3,2,11,4,66,7,6,5,677,0]
+
+let i, j, temp
+const bubbleSort=(arr)=>{
+    for(let i=0; i< arr.length; i++){
+   
+    for(let j=0; j < arr.length-i -1; j++){
+       
+        if(arr[j] > arr[j + 1]){
+            temp = arr[j]
+          arr[j]= arr[j+1]
+          arr[j+1] = temp
+        }
+    }
+}
+ return arr
+}
+console.log(bubbleSort(arr))// [ 0, 1, 2, 3,  4,  4,5, 5, 6, 7, 11, 66,677]
